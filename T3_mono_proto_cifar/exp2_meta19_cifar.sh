47ec2ca53824915b6191b657c52c214f66d25a79
QI <132769881+ChaofeiQI@users.noreply.github.com>
2024-09-02 17:52:49 +0800

    Delete T3_mono_proto_cifar directory

--- T3_mono_proto_cifar/exp2_meta19_cifar.sh
+++
@@ -1,5 +0,0 @@
-sh cmd_cifar_19/run1_pretrain.sh
-sh cmd_cifar_19/run2_1shot_meta1_train.sh
-sh cmd_cifar_19/run2_1shot_meta2_test.sh
-sh cmd_cifar_19/run3_5shot_meta1_train.sh
-sh cmd_cifar_19/run3_5shot_meta2_test.sh
